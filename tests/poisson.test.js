48a0a5123a1a94817f497bb37a16dda50f06f55f
Tom <tom.w.todd@gmail.com>
2019-03-01 18:53:32 +0000

    Rounded out test suite

--- tests/poisson.test.js
+++ tests/poisson.test.js
@@ -1,14 +1,16 @@
-import {getInterval} from "../assets/lib/generateIntervals.js";
+import {getInterval, getLambda, generateIntervals} from "../assets/lib/generateIntervals.js";
 
 
-test('adds using built in add', ()=>{
 
-    expect(1+2).toBe(3);
+test('Test that getInterval gives a number greater than 0', ()=>{
+    expect(getInterval(4)).toBeGreaterThan(0);
 });
 
-// test('Just runs getIntervals', ()=>{
-//     console.log(poisson.getLambda(1,2));
-//     expect(poisson.getInterval()).toBeLessThan(1);
-//
-//
-// });
+test('Test that getLambda gives the correct value', ()=>{
+    expect(getLambda(1,2)).toBeCloseTo((1.03333),3);
+});
+
+test('Test that generateIntervals gives us a non-zero number of intervals', ()=>{
+    var intervals = generateIntervals();
+    expect(intervals.length).toBeGreaterThan(0);
+})
