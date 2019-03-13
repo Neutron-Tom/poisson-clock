1f3d569c56eaaa9bd95bcc5da361d4631d529159
Tom <tom.w.todd@gmail.com>
2019-03-13 11:59:40 +0000

    Updated the bell sound files, got paper js working

--- assets/lib/poissonClock.js
+++ assets/lib/poissonClock.js
@@ -6,23 +6,64 @@ var ring = new Howl({
     src: ['assets/sounds/veil.mp3']
 });
 
+var bells = [
+    new Howl({src: ['assets/sounds/bells/bell (1).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (2).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (3).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (4).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (5).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (6).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (7).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (8).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (9).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (10).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (11).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (12).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (13).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (14).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (15).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (16).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (17).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (18).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (19).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (20).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (21).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (22).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (23).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (24).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (25).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (26).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (27).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (28).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (29).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (30).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (31).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (32).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (33).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (34).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (35).mp3']}),
+    new Howl({src: ['assets/sounds/bells/bell (36).mp3']}),
+    ];
+
 // Start ringing
-popNWait();
+// popNWait();
 
 function basicBell(){
-    ring.play();
+    var randomBell = bells[Math.floor(Math.random() * bells.length)];
+    // ring.play();
+    randomBell.play();
 }
 
-function popNWait(bingFcn){
+export function popNWait(bingFcn){
     console.log('Bing!');
 
-    basicBell()
+    basicBell();
     var waitTime = intervals.pop();
     console.log('I will wait: '+waitTime);
     if (waitTime == undefined){
         return
     }
-    setTimeout(popNWait,waitTime*1000);
+    setTimeout(popNWait,waitTime*100);
 
 }
 
